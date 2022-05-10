c072725826103de56256bcd530c260348e4413d9
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-05-09 19:17:05 -0700

    Added cascade on delete for models

--- models/index.js
+++ models/index.js
@@ -7,23 +7,28 @@ User.hasMany(Comment, {
     foreignKey: `user_id`
 });
 Comment.belongsTo(User, {
-    foreignKey: `user_id`
+    foreignKey: `user_id`,
+    onDelete: `CASCADE`
 });
 
 // User to Post relation
 User.hasMany(Post, {
-    foreignKey: `user_id`
+    foreignKey: `user_id`,
+    onDelete: `CASCADE`
 });
 Post.belongsTo(User, {
-    foreignKey: `user_id`
+    foreignKey: `user_id`,
+    onDelete: `CASCADE`
 });
 
 // Post to comment relation
 Post.hasMany(Comment, {
-    foreignKey: `post_id`
+    foreignKey: `post_id`,
+    onDelete: `CASCADE`
 });
 Comment.belongsTo(Post, {
-    foreignKey: `post_id`
+    foreignKey: `post_id`,
+    onDelete: `CASCADE`
 });
 
 module.exports = { User, Post, Comment };
