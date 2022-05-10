6bad9932b43332889aab76d21e1d6961b3e2e003
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-05-09 17:18:19 -0700

    Added post id route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -21,4 +21,24 @@ router.get(`/`, async (req, res) => {
     }
 });
 
+router.get(`/post/:id`, async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [{
+                model: User,
+                attributes: [`name`]
+            }]});
+
+        const post = postData.get({ plain: true });
+
+        res.render(`post`, {
+            ...post,
+            logged_in: req.session.logged_in
+        });
+
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
 module.exports = router;
